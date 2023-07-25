7b7dcb26704a8b1994909abb224b124616370244
zero <zero@zero>
2023-07-25 03:01:02 +0100

    changing variable values

---
+++ 0. Hello World/hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    int amount = 10;
+
+    printf("I am a %dx Programmer now i can do a Hello World in C!", amount);
+    return 0;
+}
---
+++ 1. Variables/changing_variable_values.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int amount = 5;
+    printf("I am a %dx Programmer now i can do a Hello World in C!\n", amount);
+
+    printf("Let's change a variable's value to level up!\n");
+
+    amount = 10;
+    printf("Now I'am a %dx Programmer now change the value of variable!", amount);
+    return 0;
+}
